bacab505e77e46b50163f0c70d69b47ba7b6586f
chenguangjian.jk <chenguangjian.jk@bytedance.com>
2023-12-01 00:25:54 +0800

    2023年 12月 01日 星期五 00:25:54 CST

--- git_commit.sh
+++ git_commit.sh
@@ -9,6 +9,6 @@ git add *
 # 提交更改到仓库
 git commit -m "$msg"
 
-git remote set-url o https://Jason-Chen-2017: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@github.com/Jason-Chen-2017/note-ai.git
+git remote set-url o https://Jason-Chen-2017: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@github.com/Jason-Chen-2017/note-ai.git
 
 git push o master
